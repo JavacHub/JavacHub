3e815da387877577c706d09fcaf6332d98f91a1d
Jonny.Chang <jonny6015@icloud.com>
2020-10-17 00:31:58 +0800

    Jonny.Chong更新于20201017003151.

--- deploy.sh
+++ deploy.sh
@@ -23,8 +23,10 @@ log_info "删除成功！"
 log_info "开始编译项目："
 hugo --buildFuture
 log_info "编译完成！"
-log_info "删除自动成功的非子项目："
-git rm --cached 'public/resume'
+log_info "删除 git 相关文件："
+rm -rf public/resume/.git
+rm -rf public/resume/.gitignore
+# git rm --cached 'public/resume'
 log_info "开始推送项目："
 git add -A
 git commit -m "${CUR_USER}更新于${date}."
