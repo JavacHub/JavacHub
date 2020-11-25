d529bb4e6f11c737ab25b87b9d4983df0aedaded
Jonny Chang <jonny6015@icloud.com>
2020-11-26 00:31:20 +0800

    jonny 更新于 2020年11月26日 星期四 00时31分14秒 CST.

--- update.sh
+++ update.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 date=$(date)
-echo -n "Push Message： "
-read  message
+echo "当前时间是： ${date}"
+echo -n "请填写推送消息并回车(默认为时间)： "
+read message
 if [ "$message"x != ""x ];then
 	message="$message;　"
 fi
 git pull --rebase --autostash
 git add . -A
 git commit -m "${message}${USER} 更新于 ${date}."
+echo "提交成功，推送至远程...\n"
 git push --force origin master
 
-echo "更新并推送成功！"
+echo "*~*~*~*~*~*~*~*~*~*~*~*"
+echo "    更新并推送成功！"
+echo "*~*~*~*~*~*~*~*~*~*~*~*"
 exit 0;
